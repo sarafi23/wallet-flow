24fcee41945782fa706cc317bf2f1ffe170ab898
Alberto <albertorzb17@agmail.com>
2026-02-01 19:15:14 -0500

    feat: modelos de finanzas y configuración de admin

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria, Transaccion
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tipo', 'icono')
+
+@admin.register(Transaccion)
+class TransaccionAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'categoria', 'monto', 'fecha')
+    list_filter = ('fecha', 'categoria')
